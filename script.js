900b39a7ec1a47aab84d3a81f760c62f2f8feef5
marycross3 <marycradick@gmail.com>
2023-03-02 20:01:42 -0600

    first bonus

--- script.js
+++ script.js
@@ -1,1 +1,38 @@
 // TODO: add code here
+window.addEventListener("load", function(){
+    const div = document.getElementById('container');
+
+    fetch("https://handlers.education.launchcode.org/static/astronauts.json").then(function(response){
+        response.json().then(function(json){
+            json.sort(function(a,b){
+                return b.hoursInSpace - a.hoursInSpace
+            });
+            // if (json.active == true ){
+            //     document.getElementById("active").style.color= "green";
+            // }
+            for (let i= 0; i < json.length;i++){
+                
+                div.innerHTML += 
+                `
+                    <div class = "astronaut">
+                    <div class = "bio">
+                        <h3>${json[i].firstName} ${json[i].lastName} </h3>
+                        <ul>
+                            <li>Hours in space: ${json[i].hoursInSpace}</li>
+                            <li>Active: ${json[i].active}</li>
+                            <li>Skills: ${json[i].skills.join(", ")} </li>
+                        </ul>
+                    </div>
+                    <img class="avatar" src= "${json[i].picture}"></img>
+                </div>`
+            }
+            
+            
+        });
+
+
+        
+    });
+    
+})
+
